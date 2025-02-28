acc52468b6526a2d37a9f2b4b54764715ad831d2
jmii1236 <148896255+jmii1236@users.noreply.github.com>
2025-02-28 03:15:22 -0500

    Add files via upload

---
+++ user/src/pages/dashboard.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+
+const Dashboard = () => {
+
+
+  return (
+    <div className='dashboard'>
+      {/* <ChatContainer /> */}
+      <div className='swiper'>
+        <div className='card'>   
+
+
+        </div>
+      </div>
+    </div>
+  )
+
+
+}
+
+export default Dashboard
