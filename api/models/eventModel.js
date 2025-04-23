965300062c9581549f96c9220a0855b9f1e2deb9
jmii1236 <148896255+jmii1236@users.noreply.github.com>
2025-04-23 01:09:03 -0400

    Create eventModel.js

---
+++ api/models/eventModel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    creatorName: {
+      type: String,
+      required: true,
+    },
+    attendees: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model("Event", eventSchema);
+
+export default Event;
